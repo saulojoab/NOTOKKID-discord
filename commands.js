115bf0bc01f8f806e903fb9b85cc9189696b8b50
saulojoab <saulojoabps@gmail.com>
2020-08-02 23:52:22 -0300

    sending redes on dm

--- commands.js
+++ commands.js
@@ -11,6 +11,24 @@ const commands = {
             message.channel.send(messageToSend);
         }
     },
+    'redes': {
+        'action': (message) => {
+            message.author.send(`
+iai tudo bom? ta aqui as redes do laud ó:
+:computer: Github -- https://github.com/saulojoab
+:bird: Twitter Profissional -- https://twitter.com/saulojoab
+:bird: Twitter Pessoal -- https://twitter.com/laudtriste
+:speech_balloon: Twitch -- https://twitch.tv/laudeee
+:homes: Site -- https://saulojoab.com
+
+tem o email tambem caso queira alo@saulojoab.com
+            `).then(() => {
+                message.channel.send(`${message.author} te mandei as redes do laud na dm ok`)
+            }).catch((e) => {
+                message.channel.send(`${message.author} não consigo te mandar dm pra mandar as redes :(`)
+            }); 
+        }
+    },
 }
 
 module.exports = commands;
--- index.js
+++ index.js
@@ -8,14 +8,14 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
+client.on('message', async message => {
     if (message.member.id === client.user.id) {
         return;
     }
 
     if (message.content.startsWith('!')) {
         const data = commands[message.content.split(' ')[0].slice(1)]; // getting the command without the !
-        
+
         if (data) {
             data['action'](message);
             return;
